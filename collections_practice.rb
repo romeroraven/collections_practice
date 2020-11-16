3465fbbc91734b30fe29af755aee2a35b066db2a
romeroraven <romeroravenr@gmail.com>
2020-11-15 19:19:44 -0700

    commit tree 94eac2742b06cb04aad054caed9b5793eb0e3d2e

--- collections_practice.rb
+++ collections_practice.rb
@@ -1,1 +1,53 @@
+def sort_array_asc(array)
+ array.sort {|a, b| a <=> b} 
 
+end 
+
+def sort_array_desc(array)
+    array.sort {|x,y| -(x <=> y)}
+
+end 
+
+def sort_array_char_count(array)
+    array = array.sort_by {|x| x.length}
+
+end
+
+def swap_elements(array)
+    array[1], array[2] = array[2], array[1]
+    array
+
+end
+
+def reverse_array(array)
+array.reverse
+
+end
+
+def kesha_maker(array)
+    array.each do |i|
+        i[2] = "$"
+    end
+end
+
+def find_a(array)
+    array.select do |i|
+        i.start_with? ("a") 
+    
+    end
+end 
+
+def sum_array(array)
+    array.inject {|sum, i| sum + i} 
+
+end
+
+def add_s(array)
+    array.each_with_index.collect do |element, index|
+        if index != 1
+            element + "s"
+        else 
+            element
+        end
+    end
+end 
